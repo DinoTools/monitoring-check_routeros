46d744c214d9b48ba841042737b1a9d9869b9f12
PhiBo <phibo@dinotools.org>
2021-12-29 22:51:46 +0100

    Add system memory check

--- check_routeros.py
+++ check_routeros.py
@@ -5,7 +5,7 @@
 import logging
 import re
 import ssl
-from typing import Any, Dict, Optional, Tuple
+from typing import Any, Dict, List, Optional, Tuple
 
 import click
 import librouteros
@@ -72,6 +72,45 @@ class RouterOSCheckResource(nagiosplugin.Resource):
             **extra_kwargs
         )
         return api
+
+
+class ScalarPercentContext(nagiosplugin.ScalarContext):
+    def __init__(self, name, total_name: str, warning=None, critical=None,
+                 fmt_metric='{name} is {valueunit}', result_cls=nagiosplugin.Result):
+        super(ScalarPercentContext, self).__init__(name, fmt_metric=fmt_metric, result_cls=result_cls)
+
+        self._warning = warning
+        self._critical = critical
+        self._total_name = total_name
+        self.warning = None
+        self.critical = None
+
+    def _prepare_ranges(self, metric, resource):
+        def replace(m):
+            if m.group("unit") == "%":
+                return str(float(total_value) * (float(m.group("value")) / 100))
+            else:
+                raise ValueError("Unable to convert type")
+
+        if self.warning is not None and self.critical is not None:
+            return
+
+        total_value = getattr(resource, self._total_name)
+        regex = re.compile(r"(?P<value>\d+)(?P<unit>[%])")
+
+        print(regex.sub(replace, self._warning))
+        print(regex.sub(replace, self._critical))
+
+        self.warning = nagiosplugin.Range(regex.sub(replace, self._warning))
+        self.critical = nagiosplugin.Range(regex.sub(replace, self._critical))
+
+    def evaluate(self, metric, resource):
+        self._prepare_ranges(metric, resource)
+        return super(ScalarPercentContext, self).evaluate(metric, resource)
+
+    def performance(self, metric, resource):
+        self._prepare_ranges(metric, resource)
+        return super(ScalarPercentContext, self).performance(metric, resource)
 
 
 @click.group()
@@ -208,6 +247,103 @@ def interface_vrrp(ctx, name, master):
         InterfaceVrrpMaster("master"),
         BooleanContext("running")
     )
+
+    check.main(verbose=ctx.obj["verbose"])
+
+
+#########################
+# System Memory         #
+#########################
+class SystemMemoryResource(RouterOSCheckResource):
+    name = "MEMORY"
+
+    def __init__(self, cmd_options):
+        super().__init__(cmd_options=cmd_options)
+
+        self.memory_total = None
+
+    def probe(self):
+        api = self._connect_api()
+
+        logger.info("Fetching data ...")
+        call = api.path(
+            "/system/resource"
+        ).select(
+            librouteros.query.Key("free-memory"),
+            librouteros.query.Key("total-memory")
+        )
+        results = tuple(call)
+        result = results[0]
+
+        memory_free = result["free-memory"]
+        self.memory_total = result["total-memory"]
+
+        yield nagiosplugin.Metric(
+            name="free",
+            value=memory_free,
+            uom="B",
+            min=0,
+            max=self.memory_total,
+        )
+
+        yield nagiosplugin.Metric(
+            name="used",
+            value=self.memory_total - memory_free,
+            uom="B",
+            min=0,
+            max=self.memory_total,
+        )
+
+
+class SystemMemorySummary(nagiosplugin.summary.Summary):
+    def __init__(self, result_names: List[str]):
+        super().__init__()
+        self._result_names = result_names
+
+    def ok(self, results):
+        msgs = []
+        for result_name in self._result_names:
+            msgs.append(str(results[result_name]))
+        return " ".join(msgs)
+
+
+@cli.command("system.memory")
+@click.option("--used/--free", is_flag=True, default=True)
+@click.option("--warning", required=True)
+@click.option("--critical", required=True)
+@click.pass_context
+@nagiosplugin.guarded
+def system_memory(ctx, used, warning, critical):
+    check = nagiosplugin.Check(
+        SystemMemoryResource(
+            cmd_options=ctx.obj,
+        )
+    )
+
+    if used:
+        check.add(nagiosplugin.ScalarContext(
+            name="free",
+        ))
+        check.add(ScalarPercentContext(
+            name="used",
+            total_name="memory_total",
+            warning=warning,
+            critical=critical
+        ))
+    else:
+        check.add(ScalarPercentContext(
+            name="free",
+            total_name="memory_total",
+            warning=f"{warning}:",
+            critical=f"{critical}:"
+        ))
+        check.add(nagiosplugin.ScalarContext(
+            name="used",
+        ))
+
+    check.add(SystemMemorySummary(
+        result_names=["used"] if used else ["free"]
+    ))
 
     check.main(verbose=ctx.obj["verbose"])
 

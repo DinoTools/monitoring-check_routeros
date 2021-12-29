742dc11a33aeee2c8770e5b64d1f4a0456b1127d
PhiBo <phibo@dinotools.org>
2021-12-29 23:31:05 +0100

    Add initial ospf neighbor check

--- check_routeros.py
+++ check_routeros.py
@@ -26,6 +26,7 @@ class BooleanContext(nagiosplugin.Context):
 class RouterOSCheckResource(nagiosplugin.Resource):
     def __init__(self, cmd_options: Dict[str, Any]):
         self._cmd_options = cmd_options
+        self._routeros_metric_values: List[Dict[str, Any]] = []
 
     def _connect_api(self) -> librouteros.api.Api:
         def wrap_socket(socket):
@@ -72,6 +73,43 @@ class RouterOSCheckResource(nagiosplugin.Resource):
             **extra_kwargs
         )
         return api
+
+    @staticmethod
+    def parse_routeros_time(time_string: str):
+        m = re.compile(r"(?P<hours>\d+)h(?P<minutes>\d+)m(?P<seconds>\d+)s").match(time_string)
+        if not m:
+            raise ValueError("Unable to parse time")
+
+        return int(m.group("hours")) * 60 * 60 + \
+            int(m.group("minutes")) * 60 + \
+            int(m.group("seconds"))
+
+    def get_routeros_select_keys(self) -> List[librouteros.query.Key]:
+        keys = []
+        for metric_value in self._routeros_metric_values:
+            keys.append(librouteros.query.Key(metric_value["name"]))
+        return keys
+
+    def get_routeros_metrics(self, result: Dict[str, Any]) -> List[nagiosplugin.Metric]:
+        metrics = []
+        for metric_value in self._routeros_metric_values:
+            value = result[metric_value["name"]]
+            if metric_value["type"] is not None:
+                value = metric_value["type"](value)
+
+            extra_kwargs = {}
+            for n in ("min", "max", "uom"):
+                if n in metric_value:
+                    extra_kwargs[n] = metric_value[n]
+
+            metrics.append(
+                nagiosplugin.Metric(
+                    name=metric_value.get("dst", metric_value["name"]),
+                    value=value,
+                    **extra_kwargs,
+                )
+            )
+        return metrics
 
 
 class ScalarPercentContext(nagiosplugin.ScalarContext):
@@ -246,6 +284,119 @@ def interface_vrrp(ctx, name, master):
         InterfaceVrrpInvalid("invalid"),
         InterfaceVrrpMaster("master"),
         BooleanContext("running")
+    )
+
+    check.main(verbose=ctx.obj["verbose"])
+
+
+################################
+# Check: Routing OSPF Neighbor #
+################################
+class RoutingOSPFNeighborResource(RouterOSCheckResource):
+    name = "OSPF NEIGHBOR"
+
+    def __init__(
+            self,
+            cmd_options: Dict[str, Any],
+            instance: str,
+            router_id: str,
+            area: Optional[str] = None
+    ):
+        super().__init__(cmd_options=cmd_options)
+
+        self.area = area
+        self.instance = instance
+        self.router_id = router_id
+
+        self.state: Optional[str] = None
+
+        self._routeros_metric_values = [
+            {"name": "priority", "type": int},
+            {"name": "adjacency", "type": self.parse_routeros_time, "min": 0, "uom": "s"},
+            {"name": "state", "type": None},
+            {"name": "state-changes", "dst": "state_changes", "type": int},
+            {"name": "ls-retransmits", "dst": "ls_retransmits", "type": int},
+            {"name": "ls-requests", "dst": "ls_requests", "type": int},
+            {"name": "db-summaries", "dst": "db_summaries", "type": int},
+        ]
+
+    def probe(self):
+        # ToDo: Only available in v7.x
+        # key_area = librouteros.query.Key("area")
+        key_instance = librouteros.query.Key("instance")
+        key_router_id = librouteros.query.Key("router-id")
+
+        api = self._connect_api()
+
+        logger.info("Fetching data ...")
+
+        call = api.path(
+            "/routing/ospf/neighbor"
+        ).select(
+            key_instance,
+            # key_area,
+            key_router_id,
+            *self.get_routeros_select_keys()
+        ).where(
+            key_instance == self.instance,
+            # key_area == self._area,
+            key_router_id == self.router_id
+        )
+        results = tuple(call)
+        if len(results) == 0:
+            return nagiosplugin.Metric(
+                name="state",
+                value=None
+            )
+
+        result = results[0]
+
+        return self.get_routeros_metrics(result)
+
+
+class RoutingOSPFNeighborState(BooleanContext):
+    def evaluate(self, metric, resource: InterfaceVrrpCheck):
+        if metric.value is None:
+            return nagiosplugin.Result(
+                state=nagiosplugin.state.Critical,
+                hint=f"Neighbor for instance '{resource.instance}' and router-id '{resource.router_id}' not found"
+            )
+        elif metric.value in ("Down",):
+            return self.result_cls(
+                state=nagiosplugin.state.Critical,
+                hint="Linkt to neighbor down"
+            )
+        elif metric.value in ("Full",):
+            return self.result_cls(
+                state=nagiosplugin.state.Ok,
+                hint="Communicating with neighbor"
+            )
+        else:
+            return self.result_cls(
+                state=nagiosplugin.state.Warn,
+                hint=f"Link to neighbor not fully up, state: {metric.value}"
+            )
+
+
+@cli.command("routing.ospf.neighbors")
+@click.option("--instance", required=True)
+@click.option("--router-id", required=True)
+@click.pass_context
+def routing_ospf_neighbors(ctx, instance, router_id):
+    resource = RoutingOSPFNeighborResource(
+        cmd_options=ctx.obj,
+        instance=instance,
+        router_id=router_id,
+    )
+    check = nagiosplugin.Check(
+        resource,
+        nagiosplugin.ScalarContext("priority"),
+        nagiosplugin.ScalarContext("adjacency"),
+        nagiosplugin.ScalarContext("state_changes"),
+        nagiosplugin.ScalarContext("ls_retransmits"),
+        nagiosplugin.ScalarContext("ls_requests"),
+        nagiosplugin.ScalarContext("db_summaries"),
+        RoutingOSPFNeighborState("state")
     )
 
     check.main(verbose=ctx.obj["verbose"])

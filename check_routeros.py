a48c36a7b7b490bc0d6de45efa153a6f9fd42b6b
PhiBo <phibo@dinotools.org>
2022-10-13 15:54:45 +0200

    src - New command to check system temperature

--- check_routeros.py
+++ check_routeros.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 from datetime import datetime
 import logging
+from pprint import pformat
 import re
 import ssl
 from typing import Any, Dict, List, Optional, Tuple, Union
@@ -1327,6 +1328,164 @@ def system_psu(ctx, warning_values, critical_values):
         nagiosplugin.Result(
             nagiosplugin.state.Ok,
             hint="Looks like all PSU work like expected"
+        )
+    )
+
+    check.main(verbose=ctx.obj["verbose"])
+
+
+#########################
+# System Temperature    #
+#########################
+class SystemTemperatureResource(RouterOSCheckResource):
+    name = "Temperature"
+
+    def __init__(
+        self,
+        cmd_options,
+        check: nagiosplugin.Check,
+        warning_values: List[str],
+        critical_values: List[str],
+        use_regex: bool
+    ):
+        super().__init__(cmd_options=cmd_options)
+
+        self._check = check
+
+        self.values: Dict[str, float] = {}
+        self.use_regex: bool = use_regex
+
+        self.warning_values: Dict[str, str] = {}
+        self.critical_values: Dict[str, str] = {}
+        self.warning_regex_values: Dict[re.Pattern, str] = {}
+        self.critical_regex_values: Dict[re.Pattern, str] = {}
+
+        if self.use_regex:
+            self.warning_regex_values = self._prepare_regex_thresholds(warning_values)
+            self.critical_regex_values = self._prepare_regex_thresholds(critical_values)
+        else:
+            self.warning_values = self._prepare_thresholds(warning_values)
+            self.critical_values = self._prepare_thresholds(critical_values)
+
+    @staticmethod
+    def _prepare_thresholds(thresholds: List[str]):
+        results = {}
+        for threshold in thresholds:
+            name, _, value = threshold.partition(":")
+            if value is None or value == "":
+                logger.warning(f"Unable to parse threshold for {name}")
+            results[name] = value
+        return results
+
+    @staticmethod
+    def _prepare_regex_thresholds(thresholds: List[str]):
+        results = {}
+        for threshold in thresholds:
+            name, _, value = threshold.partition(":")
+            if value is None or value == "":
+                logger.warning(f"Unable to parse threshold for {name}")
+            results[re.compile(name)] = value
+        return results
+
+    def probe(self):
+        api = self._connect_api()
+
+        logger.info("Fetching data ...")
+        call = api.path(
+            "/system/health"
+        )
+        results = tuple(call)
+        result = results[0]
+        logger.debug(f"Extracted values {pformat(result)}")
+
+        regex_name = re.compile(r".*temperature.*")
+        for name, value in result.items():
+            m = regex_name.match(name)
+            if not m:
+                continue
+
+            if self.use_regex:
+                for regex, threshold in self.warning_regex_values.items():
+                    if regex.match(name):
+                        self.warning_values[name] = threshold
+                        break
+
+                for regex, threshold in self.critical_regex_values.items():
+                    if regex.match(name):
+                        self.critical_values[name] = threshold
+                        break
+
+            self.values[name] = float(value)
+
+        for name, value in self.values.items():
+            self._check.add(nagiosplugin.ScalarContext(
+                name=name,
+                warning=self.warning_values.get(name),
+                critical=self.critical_values.get(name),
+            ))
+            yield nagiosplugin.Metric(
+                name=name,
+                value=value,
+            )
+
+
+@cli.command("system.temperature")
+@click.option(
+    "warning_values",
+    "--value-warning",
+    multiple=True,
+    help=(
+        "Set a warning threshold for a value. "
+        "Example: If cpu-temperature should be in the range of 40 and 60°C you can set "
+        "--value-warning cpu-temperature:40:60 "
+        "If cpu-temperature should not be higher than 50.5°C you can set "
+        "--value-warning cpu-temperature:50.5 "
+        "Can be specified multiple times"
+    )
+)
+@click.option(
+    "critical_values",
+    "--value-critical",
+    multiple=True,
+    help=(
+        "Set a critical threshold for a value. "
+        "Example: If cpu-temperature should be in the range of 40 and 60°C you can set "
+        "--value-critical cpu-temperature:40:60 "
+        "If cpu-temperature should not be higher than 50.5°C you can set "
+        "--value-critical cpu-temperature:50.5 "
+        "Can be specified multiple times"
+    )
+)
+@click.option(
+    "--regex",
+    "use_regex",
+    default=False,
+    is_flag=True,
+    help="Treat values from --value-warning and --value-critical as regex to find all matching values"
+)
+@click.pass_context
+@nagiosplugin.guarded
+def system_temperature(ctx, warning_values, critical_values, use_regex):
+    """This command reads the information from /system/health and extracts all values containing the
+    word temperature in its name. Like 'board-temperature', 'board-temperature1', 'cpu-temperature', ...
+
+    Be aware that not all devices return the same values.
+    """
+    check = nagiosplugin.Check()
+
+    temperature_resource = SystemTemperatureResource(
+        cmd_options=ctx.obj,
+        check=check,
+        warning_values=warning_values,
+        critical_values=critical_values,
+        use_regex=use_regex,
+    )
+    check.add(temperature_resource)
+
+    check.results.add(
+        nagiosplugin.Result(
+            nagiosplugin.state.Ok,
+            hint="Looks like all temperatures are OK"
         )
     )
 

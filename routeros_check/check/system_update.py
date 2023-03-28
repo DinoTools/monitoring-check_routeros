577edfe70dd3c379ad2a8bd1c137f5946487fd8c
PhiBo <phibo@dinotools.org>
2023-03-28 12:01:14 +0200

    src - Add support for system.update (Ref: #32)

---
+++ routeros_check/check/system_update.py
@@ -0,0 +1,130 @@
+# SPDX-FileCopyrightText: PhiBo DinoTools (2021)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from typing import List, Optional
+
+import click
+import nagiosplugin
+
+from ..cli import cli
+from ..helper import logger, RouterOSVersion
+from ..resource import RouterOSCheckResource
+
+
+class SystemUpdateResource(RouterOSCheckResource):
+    name = "Update"
+
+    def __init__(
+        self,
+        cmd_options,
+        check: nagiosplugin.Check,
+    ):
+        super().__init__(cmd_options=cmd_options)
+
+        self._check = check
+        self._installed_version = None
+        self._latest_version = None
+
+    def probe(self):
+        logger.info("Fetching data ...")
+        call = self.api.path(
+            "/system/package/update"
+        )
+        result = tuple(call)[0]
+
+        self._routeros_metric_values = [
+            {"name": "channel", "type": None},
+        ]
+
+        installed_version = result.get("installed-version")
+        if installed_version:
+            self._installed_version = RouterOSVersion(installed_version)
+            self._check.results.add(
+                nagiosplugin.Result(
+                    nagiosplugin.Ok,
+                    hint=f"Installed version: {self._installed_version}"
+                )
+            )
+        else:
+            self._check.results.add(
+                nagiosplugin.Result(
+                    nagiosplugin.Warn,
+                    hint="Unable to get installed version"
+                )
+            )
+
+        latest_version = result.get("latest-version")
+        if latest_version:
+            self._latest_version = RouterOSVersion(latest_version)
+
+        if self._installed_version and self._latest_version:
+            if self._installed_version < self._latest_version:
+                self._check.results.add(
+                    nagiosplugin.Result(
+                        nagiosplugin.Critical,
+                        hint=(
+                            f"Update version '{self._latest_version}' available. "
+                            f"Version installed '{self._installed_version}'"
+                        )
+                    )
+                )
+
+        return self.get_routeros_metric_item(result)
+
+
+class SystemUpdateChannelContext(nagiosplugin.Context):
+    def __init__(self, *args, channels: Optional[List[str]] = None, **kwargs):
+        super(SystemUpdateChannelContext, self).__init__(*args, **kwargs)
+        self._channels = channels
+
+    def evaluate(self, metric, resource):
+        if self._channels is None or len(self._channels) == 0 or metric.value in self._channels:
+            return nagiosplugin.Result(
+                nagiosplugin.Ok,
+                hint=f"Update channel is '{metric.value}'"
+            )
+
+        return nagiosplugin.Result(
+            nagiosplugin.Warn,
+            hint=f"Update channel '{metric.value}' not in list with allowed channels: {', '.join(self._channels)}"
+        )
+
+
+class SystemUpdateSummary(nagiosplugin.Summary):
+    def ok(self, results: List[nagiosplugin.Result]):
+        messages = []
+        for result in results:
+            messages.append(result.hint)
+
+        if len(messages) > 0:
+            return ", ".join(messages)
+
+        return "Looks good"
+
+
+@cli.command("system.update")
+@click.option(
+    "--channel",
+    "channels",
+    default=None,
+    multiple=True,
+    help="Allowed update channel. Repeat to use multiple values."
+)
+@click.pass_context
+@nagiosplugin.guarded
+def system_license(ctx, channels):
+    check = nagiosplugin.Check()
+
+    check.add(
+        SystemUpdateResource(
+            cmd_options=ctx.obj,
+            check=check,
+        ),
+        SystemUpdateChannelContext(
+            name="channel",
+            channels=channels,
+        ),
+        SystemUpdateSummary(),
+    )
+
+    check.main(verbose=ctx.obj["verbose"])

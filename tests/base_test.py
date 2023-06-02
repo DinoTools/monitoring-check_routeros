04e93caa5515d9877492c2057b274c841f344199
PhiBo <phibo@dinotools.org>
2023-06-02 11:48:58 +0200

    src - Use the right values in the tests

--- tests/base_test.py
+++ tests/base_test.py
@@ -27,7 +27,8 @@ class TestBase:
 
     def test_parse_routeros_time(self):
         check = RouterOSCheckResource(cmd_options={})
-        assert check.parse_routeros_time_duration("13m53s460ms") == 2
+        assert check.parse_routeros_time_duration("-8ms15us") == -0.008015
+        assert check.parse_routeros_time_duration("13m53s460ms") == 833.46
         assert check.parse_routeros_time_duration("1s") == 1
         assert check.parse_routeros_time_duration("2m1s") == 1 + 2 * 60
         assert check.parse_routeros_time_duration("3h2m1s") == 1 + 2 * 60 + 3 * 60 * 60
